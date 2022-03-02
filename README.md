1009d79043b3c616bb0b8c294c199f17f3f70d66
Xander Moffatt <xmoffatt@instructure.com>
2022-03-02 16:09:51 -0700

    consolidate xml_builder into index and docs
    * include docs in both README and index page

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# Welcome to Remix!
+# LTI 1.1 Test Tool
+
+This test tool is very barebones and has only the most basic functionality needed
+for testing Canvas's LTI 1.1 support.
+
+## Canvas Installation and Usage
+
+Navigate to https://lti-1p1-test-tool.vercel.app/ and configure your tool
+the way you want it. Fields left blank will use defaults. Course Navigation is the
+only default placement, so add more if you want them. Once you're ready, copy the
+XML from the bottom of the page.
+
+In a Canvas course or account, navigate to Settings -> Apps -> + App -> Paste XML.
+Give the tool a name (I prefer LTI 1.1 Test Tool) and put `key` for the Consumer
+Key and `secret` for the Shared Secret, and paste the XML in the XML box.
+
+Save the tool and then reload the page. `LTI 1.1 Test Tool` should be a new option
+in the course nav sidebar. Launch it and you will be able to see all the launch
+parameters.
+
+Built with Remix
 
 - [Remix Docs](https://remix.run/docs)
 
@@ -11,43 +31,11 @@ npm run dev
 ```
 
 This starts your app in development mode, rebuilding assets on file changes.
+Note that your tool url and domain will now be `localhost:3000` instead of the
+deployed url and domain.
 
 ## Deployment
 
-First, build your app for production:
-
-```sh
-npm run build
-```
-
-Then run the app in production mode:
-
-```sh
-npm start
-```
-
-Now you'll need to pick a host to deploy it to.
-
-### DIY
-
-If you're familiar with deploying node applications, the built-in Remix app server is production-ready.
-
-Make sure to deploy the output of `remix build`
-
-- `build/`
-- `public/build/`
-
-### Using a Template
-
-When you ran `npx create-remix@latest` there were a few choices for hosting. You can run that again to create a new project, then copy over your `app/` folder to the new project that's pre-configured for your target server.
-
-```sh
-cd ..
-# create a new project, and pick a pre-configured host
-npx create-remix@latest
-cd my-new-remix-app
-# remove the new project's app (not the old one!)
-rm -rf app
-# copy your app over
-cp -R ../my-old-remix-app/app app
-```
+This tool is deployed with Vercel and is connected to the github repo. If you
+have push rights to `master`, pushing to master will deploy new code. Otherwise,
+open a pull request to get your changes merged and deployed.
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,28 +1,96 @@
-import { Link } from "remix";
+import { Form, LoaderFunction, useLoaderData } from "remix";
+import { PLACEMENTS } from "~/placements.server";
+import { buildXML, XMLOptions } from "~/xml.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const getParam = (p: string) => url.searchParams.get(p) as string;
+
+  const opts: XMLOptions = {
+    title: getParam("tool_name"),
+    description: getParam("description"),
+    domain: getParam("tool_domain"),
+    launchUrl: getParam("launch_url"),
+    privacyLevel: getParam("privacy_level"),
+    selectionHeight: getParam("selection_height"),
+    selectionWidth: getParam("selection_width"),
+  };
+  return {
+    xml: buildXML(opts),
+    placements: PLACEMENTS,
+  };
+};
+
+type FieldProps = {
+  name: string;
+  inputName: string;
+  type?: string;
+  defaultValue?: string;
+};
+const Field = ({
+  name,
+  inputName,
+  type = "string",
+  defaultValue = "",
+}: FieldProps) => (
+  <tr>
+    <td>{name}</td>
+    <td>
+      <input name={inputName} type={type} defaultValue={defaultValue}></input>
+    </td>
+  </tr>
+);
 
 export default function Index() {
+  const { xml } = useLoaderData();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>LTI 1.1 Test Tool</h1>
       <h3>Usage</h3>
       <p>
-        Build a tool configuration in XML and install it in Canvas using `key`
-        and `secret`.
+        Fill out the fields below to your heart's content to configure your
+        tool. Fields left blank will use sane defaults.
       </p>
-      <ul>
-        <li>
-          <Link to="/xml_builder">XML Builder</Link>
-        </li>
-        <li>
-          <Link to="/xml_config">XML Config</Link>
-        </li>
-        <li>
-          <Link to="/launch">Basic Launch</Link>
-        </li>
-        <li>
-          <Link to="/content_item_launch">Content Item Launch</Link>
-        </li>
-      </ul>
+      <p>
+        Course Navigation is the only default placement, so add more if you want
+        them. Once you're ready, copy the XML from the bottom of the page.
+      </p>
+      <p>
+        In a Canvas course or account, navigate to Settings -&gt; Apps -&gt; +
+        App -&gt; Paste XML. Give the tool a name (I prefer LTI 1.1 Test Tool)
+        and put `key` for the Consumer Key and `secret` for the Shared Secret,
+        and paste the XML in the XML box.
+      </p>
+      <p>
+        Save the tool and then reload the page. `LTI 1.1 Test Tool` should be a
+        new option in the course nav sidebar. Launch it and you will be able to
+        see all the launch parameters.
+      </p>
+      <h3>Configuration</h3>
+      <Form method="get">
+        <table>
+          <Field name="Tool Name" inputName="tool_name"></Field>
+          <Field name="Description" inputName="description"></Field>
+          <Field name="Tool Domain" inputName="tool_domain"></Field>
+          <Field name="Launch URL" inputName="launch_url"></Field>
+          <Field name="Privacy Level" inputName="privacy_level"></Field>
+          <Field
+            name="Selection Height"
+            inputName="selection_height"
+            type="number"
+            defaultValue="500"
+          ></Field>
+          <Field
+            name="Selection Width"
+            inputName="selection_width"
+            type="number"
+            defaultValue="500"
+          ></Field>
+        </table>
+        <button type="submit">Create</button>
+      </Form>
+      <h3>Generated XML</h3>
+      <code>${xml}</code>
     </div>
   );
 }
