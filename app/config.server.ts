2d32a12a671c933672bb7d455506098f271e8c9c
Xander Moffatt <xmoffatt@instructure.com>
2022-03-02 14:44:22 -0700

    final domain tweaks

--- app/config.server.ts
+++ app/config.server.ts
@@ -1,5 +1,5 @@
 export default function config() {
-  const domain = process.env.LTI_DOMAIN || "localhost";
+  const domain = process.env.LTI_DOMAIN || "localhost:3000";
   const prefix = process.env.NODE_ENV === "production" ? "https://" : "http://";
   const launchURL = `${prefix}${domain}/launch`;
 
