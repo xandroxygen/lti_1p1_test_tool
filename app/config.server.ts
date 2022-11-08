f5e01aa0b47b2c0d28c63102034e238fd89ad2f1
Xander Moffatt <xmoffatt@instructure.com>
2022-11-08 13:36:06 -0500

    add 1.3 things to config file

--- app/config.server.ts
+++ app/config.server.ts
@@ -2,12 +2,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default function config() {
-  const domain = process.env.LTI_DOMAIN;
+  const DOMAIN = process.env.LTI_DOMAIN;
   const prefix = process.env.NODE_ENV === "production" ? "https://" : "http://";
-  const launchURL = `${prefix}${domain}/launch`;
+  const baseUrl = `${prefix}${DOMAIN}`;
+  const LAUNCH_URL = `${baseUrl}/launch`;
+  const LAUNCH_URL_1_3 = `${baseUrl}/1_3/launch`;
+  const JWK_URL = `${baseUrl}/1_3/jwks`;
+  const OIDC_INITIATION_URL = `${baseUrl}/1_3/login`;
+  const PUBLIC_JWK = JSON.parse(process.env.PUBLIC_JWK || "{}");
+  const OIDC_AUTH_REQUEST_URL = process.env.OIDC_AUTH_REQUEST_URL || "";
+  const PLATFORM_JWKS_URL = process.env.PLATFORM_JWKS_URL || "";
 
   return {
-    DOMAIN: domain,
-    LAUNCH_URL: launchURL,
+    DOMAIN,
+    LAUNCH_URL,
+    LAUNCH_URL_1_3,
+    PUBLIC_JWK,
+    OIDC_AUTH_REQUEST_URL,
+    JWK_URL,
+    OIDC_INITIATION_URL,
+    PLATFORM_JWKS_URL,
   };
 }
