20a7fdbc973e20c96147ec261575d6b2aa8125b7
Xander Moffatt <xmoffatt@instructure.com>
2023-04-03 11:57:57 -0600

    allow 1.1 launches from separate url
    why:
    * for testing 1.1 enviroment-specific overrides
    
    test plan:
    * install this tool in a Canvas course
    * change the course_navigation launch url to `<domain>/launch2`
    * launch the tool from the course sidebar
    * it should display the launch data as normal
    * note in the network tab that both /launch2 and /launch are rendered

---
+++ app/routes/launch2.tsx
@@ -0,0 +1,31 @@
+import { ActionFunction, Form, useActionData } from "remix";
+import { LtiLaunchParams } from "~/ltiLaunchParams";
+import { useEffect, useRef } from "react";
+import invariant from "tiny-invariant";
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  return Object.fromEntries(formData) as LtiLaunchParams;
+};
+
+/*
+ * Provides a different 1.1 launch url for testing overrides.
+ * Redirects to the standard 1.1 launch url to display the launch data.
+ */
+export default function Launch2() {
+  const data = useActionData<LtiLaunchParams>();
+  invariant(data, "expected LTI launch data");
+
+  const ref = useRef<HTMLFormElement>(null);
+  useEffect(() => {
+    ref.current?.submit();
+  });
+
+  return (
+    <Form ref={ref} action="/launch" method="post">
+      {Object.entries(data).map(([k, v]) => (
+        <input key={k} type="hidden" name={k} value={v} />
+      ))}
+    </Form>
+  );
+}
